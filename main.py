409568437698b56a4b866d1c4dab0299eeec0fdc
abdou <b-abderrahmane@outlook.com>
2017-09-02 21:08:43 +0200

    added anchor fuzzing

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import urlparse
 import time
-import timeit
 import logging
 import sys
 
-from urllib import urlencode
+from urllib import quote
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-PAYLOADS = open("payloads/xss.pld").readlines()
+
 url = "http://172.16.1.203/"
 driver_path = "drivers/geckodriver"
 
@@ -34,8 +33,19 @@ def format_url_args(args_dict):
     return url_args[:-1]
 
 
-def fuzz_name_anchors():
-    pass
+def fuzz_name_anchors(link, paylods):
+    if "#" in link:
+        new_link = link.split("#")[0]
+        new_link += "#"
+        for payload in paylods:
+            fuzzed_link = new_link + payload.decode("utf-8")
+            driver.get(fuzzed_link)
+            driver.refresh()
+            time.sleep(0.5)
+            if detect_xss(driver):
+                log.info("XSS found in url : %s at anchor using payload : %s "
+                         "In order to verify it check : %s", link, payload, fuzzed_link)
+                return True
 
 
 def replace_get_arg(parsed_url, arg, new_value):
@@ -49,6 +59,15 @@ def replace_get_arg(parsed_url, arg, new_value):
     return new_url
 
 
+def detect_xss(driver):
+    try:
+        alert = driver.switch_to.alert
+        alert.accept()
+        return True
+    except Exception as e:
+        return False
+
+
 def fuzz_get_params(link, payloads):
     for payload in payloads:
         parsed = urlparse.urlparse(link)
@@ -60,16 +79,14 @@ def fuzz_get_params(link, payloads):
             )
             driver.get(new_url)
             time.sleep(0.5)
-            try:
-                alert = driver.switch_to.alert
-                alert.accept()
+            if detect_xss(driver):
                 log.info("XSS found in url : %s at arg : %s using payload : %s "
                          "In order to verify it check : %s", link, arg, payload, new_url)
                 return True
-            except Exception as e:
-                continue
+
     log.info("Finished checking [%s], no XSS found", link)
     return False
+
 
 if __name__ == '__main__':
 
@@ -79,7 +96,7 @@ if __name__ == '__main__':
     driver.implicitly_wait(2)
 
     links = get_links(driver)
-
+    xss_paylods = open("payloads/xss.pld").readlines()
     for link in links:
 
         driver.get(link)
@@ -87,7 +104,8 @@ if __name__ == '__main__':
 
         if "xss" in link:
             start = time.time()
-            fuzz_get_params(link, PAYLOADS)
+            fuzz_get_params(link, xss_paylods)
+            fuzz_name_anchors(link, xss_paylods)
             end = time.time()
             duration = end - start
             log.info("Url [%s] XXSyzed within %.2f seconds", link, duration)
