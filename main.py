19bc238fc657521ce84393aa3fd5cc457eb5ee65
abdou <b-abderrahmane@outlook.com>
2017-09-02 19:57:09 +0200

    restructured get fuzzing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import urlparse
 import time
 import timeit
+import logging
+import sys
 
 from urllib import urlencode
 from selenium import webdriver
@@ -12,6 +14,9 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 PAYLOADS = open("payloads/xss.pld").readlines()
 url = "http://172.16.1.203/"
 driver_path = "drivers/geckodriver"
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
 
 def get_forms(driver):
@@ -28,31 +33,42 @@ def format_url_args(args_dict):
         url_args += "%s=%s&" % (key, val.decode("utf-8"))
     return url_args[:-1]
 
-def fuzz_name_anchros():
+
+def fuzz_name_anchors():
     pass
+
+
+def replace_get_arg(parsed_url, arg, new_value):
+    params = {arg: new_value}
+    url_parts = list(urlparse.urlparse(link))
+    query = dict(urlparse.parse_qsl(url_parts[4]))
+    query.update(params)
+    url_parts[4] = format_url_args(query)
+    urlparse.parse_qs(parsed_url.query)[arg] = new_value
+    new_url = (urlparse.urlunparse(url_parts))
+    return new_url
+
 
 def fuzz_get_params(link, payloads):
     for payload in payloads:
         parsed = urlparse.urlparse(link)
-        print link
         for arg in urlparse.parse_qs(parsed.query).keys():
-            params = {arg: payload}
-            url_parts = list(urlparse.urlparse(link))
-            query = dict(urlparse.parse_qsl(url_parts[4]))
-            query.update(params)
-            url_parts[4] = format_url_args(query)
-            urlparse.parse_qs(parsed.query)[arg] = payload
-            new_url = (urlparse.urlunparse(url_parts))
+            new_url = replace_get_arg(
+                parsed_url=parsed,
+                arg=arg,
+                new_value=payload
+            )
             driver.get(new_url)
             time.sleep(0.5)
             try:
                 alert = driver.switch_to.alert
                 alert.accept()
-                print "SOMETHING HERE --- checked '%s' for '%s' in '%s' " % (payload, arg, new_url)
+                log.info("XSS found in url : %s at arg : %s using payload : %s "
+                         "In order to verify it check : %s", link, arg, payload, new_url)
                 return True
             except Exception as e:
                 continue
-                #print "nothing to see here --- checked '%s' for '%s' in '%s'" % (payload, arg, new_url)
+    log.info("Finished checking [%s], no XSS found", link)
     return False
 
 if __name__ == '__main__':
@@ -60,24 +76,20 @@ if __name__ == '__main__':
     driver = webdriver.Firefox(executable_path=driver_path)
 
     driver.get(url)
-    driver.implicitly_wait(1)
+    driver.implicitly_wait(2)
 
     links = get_links(driver)
 
     for link in links:
 
-        print link
         driver.get(link)
         forms = get_forms(driver)
-        if forms:
-            print link, forms
 
-        """
         if "xss" in link:
             start = time.time()
             fuzz_get_params(link, PAYLOADS)
             end = time.time()
-            print(end - start)
-            print link
-        """
+            duration = end - start
+            log.info("Url [%s] XXSyzed within %.2f seconds", link, duration)
+
     driver.close()
